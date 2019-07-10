ea06895416db1a9c2569604f196c46057b363551
osben <osben90@gmail.com>
2019-07-10 09:14:49 +0300

    add toggle Dart theme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public isDart = false;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  toggleDart() {
+    this.isDart ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.removeAttribute('data-theme');
+  }
 }
